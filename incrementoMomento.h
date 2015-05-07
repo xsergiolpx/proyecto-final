9e7dd89faee284668039f86dbc49d0823d19e71f
xsergiolpx <xsergiolpx@users.noreply.github.com>
2015-05-07 18:14:27 +0200

    Update incrementoMomento.h

--- incrementoMomento.h
+++ incrementoMomento.h
@@ -1,13 +1,13 @@
 double incrementoMomento(double x, double y, double z, double vx, double vy, double vz, double momento, double rr, double teta, double phi, double l, double m)
 {
 	if(x + rr * sin(teta)*cos(phi) > l || (x + rr * sin(teta)*cos(phi)) < 0) {
-		return (momento += 2.0 * m * vx);
+		return (momento += 2.0 * m * abs(vx));
 		//vx = -vx;
 	} else if( (y + rr * sin(teta)*sin(phi)) > l || (y + rr * sin(teta)*sin(phi)) < 0) {
-		return (momento += 2.0 * m * vy);
+		return (momento += 2.0 * m * abs(vy));
 		//vy = -vy;
 	} else if( (z + rr * cos(teta)) > l || (z + rr * cos(teta)) < 0) {
-		return (momento += 2.0 * m * vz);
+		return (momento += 2.0 * m * abs(vz));
 		//vz = -vz;
 	}
 	return momento;
